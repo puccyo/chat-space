fc4c5c5a35e6849a1f5b3fdeeb40f4d4e0f48374
puccyo <yumudo938@eay.jp>
2020-05-27 19:52:26 +0900

    [WHIP]インクリメンタルサーチ実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
 class UsersController < ApplicationController
+  def index
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+  
   def edit
   end
+  
   def update
     if current_user.update(user_params)
       redirect_to root_path
