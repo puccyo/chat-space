c47620dbb2f9cf387b06f8f7c370e1be2813c693
puccyo <yumudo938@eay.jp>
2020-05-29 17:47:32 +0900

    修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -85,7 +85,7 @@ $(function() {
       }
     })
     .fail(function() {
-      console.log("error");
+      alert("error");
     });
   };
   if (document.location.href.match(/\/groups\/\d+\/messages/)) {
