420989cc671fa8908cfcbeda023bc1322b833ef6
puccyo <yumudo938@eay.jp>
2020-05-29 17:33:22 +0900

    修正　完成

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,10 +1,9 @@
 $(function() {
-  var last_message_id = $('.message:last').data("message-id");
-  console.log(last_message_id);
+  
   function buildHTML(message){
     if ( message.image ) {
       var html =
-      `<div class="chat-main__message-list-tab">
+      `<div class="chat-main__message-list-tab" data-message-id=${message.id}>
         <div class="chat-main__message-list-tab__main">
           <div class="chat-main__message-list-tab__main__name">
             ${message.user_name}
@@ -23,7 +22,7 @@ $(function() {
     return html;
   } else {
       var html =
-        `<div class="chat-main__message-list-tab">
+        `<div class="chat-main__message-list-tab" data-message-id=${message.id}>
           <div class="chat-main__message-list-tab__main">
             <div class="chat-main__message-list-tab__main__name">
               ${message.user_name}
@@ -41,6 +40,7 @@ $(function() {
       return html;
     };
   }
+  
   $('#new_message').on('submit',function(e) {
     e.preventDefault()
     var formData = new FormData(this);
@@ -65,5 +65,31 @@ $(function() {
       $('input').prop('disabled', false);
     });
   });
+
+  var reloadMessages = function() {
+    var last_message_id = $('.chat-main__message-list-tab:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML ="";
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.chat-main__message-list').append(insertHTML);
+        $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      console.log("error");
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
 
