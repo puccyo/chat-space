ed908e855f11d558fd1108f72f2edbd4eeb4c44d
puccyo <yumudo938@eay.jp>
2020-05-30 11:56:42 +0900

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -6,7 +6,7 @@ working_directory app_path
 
 pid "#{app_path}/tmp/pids/unicorn.pid"
 
-llisten "#{app_path}/tmp/sockets/unicorn.sock"
+listen "#{app_path}/tmp/sockets/unicorn.sock"
 
 stderr_path "#{app_path}/log/unicorn.stderr.log"
 
